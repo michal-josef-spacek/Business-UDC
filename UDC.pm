7baf30cf2771bdaf54bb1634655d3d0b2d6e348d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-03-25 10:20:07 +0100

    Add main doc

--- UDC.pm
+++ UDC.pm
@@ -71,3 +71,130 @@ sub tokens {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Business::UDC - Class to work with Universal Decimal Classification.
+
+=head1 SYNOPSIS
+
+ use Business::UDC;
+
+ my $obj = Business::UDC->new($udc_string);
+ my $ast_hr = $obj->ast;
+ my $error = $obj->error;
+ my $is_valid = $obj->is_valid;
+ my $source = $obj->source;
+ my $tokens_ar = $obj->tokens;
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = Business::UDC->new($udc_string);
+
+Constructor.
+
+Returns instance of object.
+
+=head2 C<ast>
+
+ my $ast_hr = $obj->ast;
+
+Get abstract syntax tree.
+
+Returns reference to hash with structure.
+
+=head2 C<error>
+
+ my $error = $obj->error;
+
+Get error.
+
+TODO Errors.
+
+Returns string or undef.
+
+=head2 C<is_valid>
+
+ my $is_valid = $obj->is_valid;
+
+=head2 C<source>
+
+ my $source = $obj->source;
+
+Get UDC source which goes to constructor.
+
+Returns string.
+
+=head2 C<tokens>
+
+ my $tokens_ar = $obj->tokens;
+
+Get list of tokens defined by tokenization of input UDC string.
+
+Returns reference to array with tokens.
+
+=head1 EXAMPLE
+
+=for comment filename=is_valid.pl
+
+ use strict;
+ use warnings;
+
+ use Business::UDC;
+
+ if (@ARGV < 1) {
+        print STDERR "Usage: $0 udc_string\n";
+        exit 1;
+ }
+ my $udc_string = $ARGV[0];
+
+ # Object.
+ my $obj = Business::UDC->new($udc_string);
+
+ print "UDC string $udc_string ";
+ if ($obj->is_valid) {
+        print "is valid\n";
+ } else {
+        print "is not valid\n";
+ }
+
+ # Output for '821.111(73)-31"19"':
+ # UDC string 821.111(73)-31"19" is valid
+
+ # Output for '821.111(73)-31"19':
+ # UDC string 821.111(73)-31"19 is not valid
+
+=head1 DEPENDENCIES
+
+L<Business::UDC::Parser>,
+L<English>,
+L<Error::Pure>,
+L<Error::Pure::Utils>.
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Business-UDC>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2026 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
